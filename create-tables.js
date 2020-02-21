e5d988c58ef4662ea64f7518dc28423c63a16be0
Fiona Ochs <fiona.oaks@gmail.com>
2020-02-20 17:26:57 -0800

    create-table

---
+++ create-tables.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+const pg = require('pg');
+const Client = pg.Client;
+
+run();
+
+async function run() {
+    const client = new Client(process.env.DATABASE_URL);
+    //make request to gameboard database in heroku
+
+    try {
+        await client.connect();
+        //connect to database
+        await client.query(`
+            CREATE TABLE gameboards(
+                id SERIAL PRIMARY KEY NOT NULL,
+                name VARCHAR(256) NOT NULL,
+                year INTEGER,
+                image_url VARCHAR(256),
+                price INTEGER,
+                publisher VARCHAR(256),
+                categories VARCHAR(256),
+                min_players VARCHAR(256),
+                max_players VARCHAR(256),
+            );
+        `);
+    }
+    catch (err) {
+        console.log(err);
+    }
+    finally {
+        client.end();
+    }
+}
